c449fb6d05e840323a6193c506a12dfd07136a66
2LujanGonzalo <83368931+2LujanGonzalo@users.noreply.github.com>
2022-03-28 17:41:00 -0300

    Add files via upload

--- 01.cpp
+++ 01.cpp
@@ -5,11 +5,11 @@ int nro = 0;
 int suma = 0;
 
 main(){
-	for(int i = 0;i<11;i++){
+	for(int i = 0;i<10;i++){
 	printf("Ingrese un número Entero:\n");
 	scanf("%d",&nro);
 	suma+=nro;
 	}
-	printf("La suma es:%d\n");
+	printf("La suma es:%d\n",suma);
 	return 0;
 }
