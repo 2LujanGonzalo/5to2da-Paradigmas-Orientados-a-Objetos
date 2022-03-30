658d75a7e84e91bcd9b870b82dfc567dab36be40
2LujanGonzalo <83368931+2LujanGonzalo@users.noreply.github.com>
2022-03-30 12:10:43 -0300

    Add files via upload

---
+++ 05.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a[6];
+int cont=0;
+main(){
+	for(int i=0;i<6;i++){
+		printf("ingrese numeros (%d de 6)",i+1);	
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<6;i++){
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+	for(int i=0;i<6;i++){
+		if((a[i]>0 && a[i+1]>0 && a[i+2]>0) || (a[i]<0 && a[i+1]<0 && a[i+2]<0)) cont ++;
+	}
+	
+	printf("%d",cont);
+	return 0;
+}
